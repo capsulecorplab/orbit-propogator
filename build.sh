ed95171919dd39fe504c4bd23baabd71706592f1
Sean Marquez <capsulecorplab@gmail.com>
2022-08-04 17:54:10 +0000

    add build.sh for building unit tests

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+clitool="cmake"
+version=3.23.1
+cmd="$clitool .."
+dockerimage="rikorose/gcc-cmake:gcc-9"
+
+if [ ! -r ./build ]; then
+    mkdir build
+fi
+
+cd build
+
+if ! $clitool --version | grep $version > /dev/null; then
+    echo "$clitool $version not installed"
+    echo "running build via docker..."
+    docker run --rm -v "$PWD:/src" -w "/src" $dockerimage $cmd
+    make
+    ctest -V
+else
+    echo "$clitool $version installed!"
+    echo "running build..."
+    $cmd
+    make
+    ctest -V
+fi
